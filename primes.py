c27de58c5fb681ce1336a5f12a47d66afc9b4e9a
SHOZAB ANWAR SIDDIQUE <s.nadeem014@yahoo.com>
2022-12-09 13:07:23 +0000

    Attempt 1

--- primes.py
+++ primes.py
@@ -1,6 +1,28 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def primes(number_of_primes):
+def is_prime(number):
+
+    for i in range (number):
+        if (number % i != 0):
+            return False
+    return True
+
+def get_primes(number):
     list = []
+    for i in range(number):
+        if(is_prime(i) == True):
+            list.insert(i)
     return list
+
+
+
+def primes(number_of_primes):
+    if number_of_primes < 1:
+        raise ValueError('Work with Positive Numbers Only')
+    else:
+        # list = []
+        list = get_primes(number_of_primes)
+            
+        return list
+    
