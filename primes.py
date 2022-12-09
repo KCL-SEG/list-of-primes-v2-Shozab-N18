3a244f4570a523e3fc23c639944a5844651bc6cc
SHOZAB ANWAR SIDDIQUE <s.nadeem014@yahoo.com>
2022-12-09 13:22:43 +0000

    Attempt 5

--- primes.py
+++ primes.py
@@ -11,23 +11,32 @@ def is_prime(number):
     return True
 
 def get_primes(number):
+
     list = []
-    for i in range(number):
+    i = 0
+
+    while(True):
+    # for i in range(number):
+        if(len(list) == number):
+            break
         if(is_prime(i) == True):
             list.append(i)
+            i+= 1
+
     return list
 
 
 
 def primes(number_of_primes):
+    #Get first number_of_primes primes
     if number_of_primes < 1:
         raise ValueError('Work with Positive Numbers Only')
     else:
         list = []
-        temp = 0
-        while(len(list) != number_of_primes):
-            list.append(get_primes(temp))
-            temp += 1
+        # temp = 0
+        # while(len(list) != number_of_primes):
+        list.append(get_primes(number_of_primes))
+            # temp += 1
             
         return list
     
